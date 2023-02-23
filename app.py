87a067c752c58003b34e82fa48b25940b8a92aa7
sandeep kumar <sandeepkmr.cse@gmail.com>
2023-02-23 05:27:02 +0000

    Add dynamic data using pyhton list

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': ' Bengaluru, India',
+    'salary': 'Rs. 10,00000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': ' Pune, India',
+    'salary': 'Rs. 13,00000'
+  },
+  {
+    'id': 3,
+    'title': 'Data Administrator',
+    'location': ' Bengaluru, India',
+    'salary': 'Rs. 12,00000'
+  },
+  {
+    'id': 4,
+    'title': 'Project Engineer',
+    'location': ' Delhi, India',
+    'salary': 'Rs. 14,00000'
+  },
+  {
+    'id': 5,
+    'title': 'Project Engineer',
+    'location': ' Kolkata, India',
+  },
+]
+
+
 @app.route("/")
 def hello_world():
-  return "Hello World"
+  return render_template('home.html', jobs=JOBS)
 
-if __name__ =="__main__":
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
